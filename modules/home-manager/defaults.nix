f92f0da7dbefd73b19612813200d02db86d887fe
Jahn Thomas Fidje <jtfidje@gmail.com>
2024-01-02 15:56:02 +0100

    chore: Add vscode extensions

--- modules/home-manager/defaults.nix
+++ modules/home-manager/defaults.nix
@@ -1,14 +1,17 @@
 { pkgs, ... }:
 
 {
+  # Allow un-free packages
+  nixpkgs.config.allowUnfree = true;
+
   programs.alacritty = {
     enable = true;
     settings = {
       font.size = 10;
       selection.save_to_clipboard = true;
     };
-
   };
+
   programs.git = {
     enable = true;
     userEmail = "jtfidje@gmail.com";
@@ -16,6 +19,23 @@
     extraConfig = {
       init.defaultBranch = "main";
     };
+  };
+
+  programs.vscode = {
+    enable = true;
+    extensions = with pkgs.vscode-extensions; [
+      ms-python.python
+      charliermarsh.ruff
+      mhutchie.git-graph
+      bbenoist.nix
+    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        name = "vscode-theme-onedark";
+        publisher = "akamud";
+        version = "2.3.0";
+        sha256 = "1km3hznw8k0jk9sp3r81c89fxa311lc6gw20fqikd899pvhayqgh";
+      }
+    ];
   };
 
   services.picom = {
